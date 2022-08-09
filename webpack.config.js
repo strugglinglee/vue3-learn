b32d2d433a41885b0f82ccc2074bfa44d84c891b
李静(005174) <lijing.leme@fcbox.com>
2022-08-09 17:51:10 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: './packages/vue/src/index.ts',
+    output: {
+        path: path.resolve(__dirname, './packages/vue/dist'),
+        filename: 'mini-vue.cjs.js',
+    },
+    resolve: {
+        // options for resolving module requests
+        // (does not apply to resolving of loaders)
+        // modules: ["node_modules", path.resolve(__dirname, "app")],
+        // directories where to look for modules (in order)
+        extensions: [".js", ".json", ".jsx", ".css"]
+    }
+};
